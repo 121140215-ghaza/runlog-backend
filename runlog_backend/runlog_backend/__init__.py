277155236920639e0fe75b818c43a8fbb212e1be
121140215-ghaza <ghaza.121140215@student.itera.ac.id>
2025-05-24 13:54:38 +0700

    Perbaikan Bug

--- runlog_backend/runlog_backend/__init__.py
+++ runlog_backend/runlog_backend/__init__.py
@@ -1,8 +1,6 @@
 from pyramid.config import Configurator
 from pyramid.authorization import ACLAuthorizationPolicy
 from runlog_backend.auth_policy import JWTAuthenticationPolicy, get_user
-from pyramid.tweens import MAIN
-
 
 
 def main(global_config, **settings):
@@ -10,26 +8,21 @@ def main(global_config, **settings):
         config.include('pyramid_jinja2')
         config.include('runlog_backend.routes')
         config.include('runlog_backend.models')
-        
+        config.include('runlog_backend.cors') # ✅ ini doang cukup
+
         import logging
         logging.basicConfig()
         log = logging.getLogger(__name__)
         log.setLevel(logging.DEBUG)
         log.debug("Registering CORS tween")
 
-        # Jangan import, cukup daftarkan pakai path string
-        config.add_tween('runlog_backend.cors.cors_tween_factory', over=MAIN) 
-
-        # Setup JWT Authentication and Authorization
         authn_policy = JWTAuthenticationPolicy()
         authz_policy = ACLAuthorizationPolicy()
         config.set_authentication_policy(authn_policy)
         config.set_authorization_policy(authz_policy)
 
         config.add_request_method(get_user, name='user', reify=True)
-
-        # Preflight OPTIONS route
-        config.add_route('cors_preflight', '/{catch_all:.*}', request_method='OPTIONS')
+ 
 
         config.scan('runlog_backend.views')
 
--- runlog_backend/runlog_backend/cors.py
+++ runlog_backend/runlog_backend/cors.py
@@ -1,25 +1,33 @@
+from pyramid.response import Response
+
 def cors_tween_factory(handler, registry):
     def cors_tween(request):
         allowed_origin = 'http://localhost:5173'
         origin = request.headers.get('Origin')
 
         if request.method == 'OPTIONS':
-            response = request.response
-            if origin == allowed_origin:
-                response.headers['Access-Control-Allow-Origin'] = allowed_origin
-                response.headers['Access-Control-Allow-Credentials'] = 'true'
-                response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
-                response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+            response = Response()
+            if origin and allowed_origin in origin:
+                response.headers.update({
+                    'Access-Control-Allow-Origin': allowed_origin,
+                    'Access-Control-Allow-Credentials': 'true',
+                    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+                    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+                })
             response.status_code = 204
             return response
 
         response = handler(request)
-        if origin == allowed_origin:
-            response.headers['Access-Control-Allow-Origin'] = allowed_origin
-            response.headers['Access-Control-Allow-Credentials'] = 'true'
-            # optional: bisa tambahin ini biar browser gak complain
-            response.headers['Vary'] = 'Origin'
-
+        if origin and allowed_origin in origin:
+            response.headers.update({
+                'Access-Control-Allow-Origin': allowed_origin,
+                'Access-Control-Allow-Credentials': 'true',
+                'Vary': 'Origin',
+            })
         return response
 
     return cors_tween
+
+# ⬇⬇⬇ jangan lupa ini bro
+def includeme(config):
+    config.add_tween('runlog_backend.cors.cors_tween_factory')
